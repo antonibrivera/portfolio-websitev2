25b18bb92a087386fef08284845c2a4409c5072e
Joseph Rivera <antonibrivera@gmail.com>
2022-06-27 21:34:35 -0400

    added more HTML (JSX) to 'Home.js' component & commented out 'App.js' that didn't render

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
 import './App.css';
 import Home from './components/Home/Home';
 import Navbar from './components/Navbar/Navbar';
+import Resume from './components/Resume/Resume';
 import NotFound from './components/NotFound/NotFound';
 import { Routes, Route } from 'react-router-dom';
+
+// Commented out code still needs to be started in order to render to the app.
 
 function App() {
   return (
     <div className="App">
-      <Navbar />
+      {/* <Navbar /> */}
       <Routes>
         <Route exact path='/' element={<Home />} />
-        <Route path='*' element={<NotFound />} />
+        {/* <Route path='/resume' element={<Resume />} />
+        <Route path='*' element={<NotFound />} /> */}
       </Routes>
     </div>
   );
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -8,31 +8,33 @@ export default function Home(props) {
         <h1 id='hero-title'>Joseph Rivera | Software Developer</h1>
         <h3 id='hero-subtitle'>Dedicating to growing in my career and being better than I was yesterday.</h3>
       </main>
-      <section>
-        <h2>Who am I?</h2>
-        <p>Lorem ipsum dolor sit amet.</p>
+      <section className='about-section'>
+        <h2 id='about-title'>Who am I?</h2>
+        <p id='about-description'>Lorem ipsum dolor sit amet.</p>
       </section>
-      <section>
-        <h2>Projects</h2>
-        <ul>
+      <section className='projects-section'>
+        <h2 id='projects-title'>Projects</h2>
+        <ul id='projects-list'>
           ...
         </ul>
       </section>
-      <section>
-        <h2>Want to get in touch?</h2>
-        <form>
-          <label>
-            Name:
-            <input type='text' id='contact-name' />
+      <section className='contact-section'>
+        <h2 id='contact-title'>Want to get in touch?</h2>
+        <form id='contact-form'>
+          <legend>Contact Me</legend>
+          <label htmlFor='full-name'>
+            Full Name:
+            <input type='text' id='full-name' name='user_name' placeholder='Full name...' />
           </label>
-          <label>
+          <label htmlFor='email'>
             Email:
-            <input type='email' id='contact-email' />
+            <input type='email' id='email' name='user_email' placeholder='Email...' />
           </label>
-          <label>
+          <label htmlFor='message'>
             Message:
-            <textarea id='contact-message' />
+            <textarea id='message' name='user_message' placeholder='Enter a brief message...' />
           </label>
+          <button type='submit'>Submit</button>
         </form>
       </section>
     </div>
